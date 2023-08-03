1d8a50293381fa83180cb70e1521f6baf65f1792
depryf <depryf@imsweb.com>
2023-08-03 15:45:53 -0400

    Tweaks to the NCCR dictionary

--- src/test/java/lab/NccrDictionaryLab.java
+++ src/test/java/lab/NccrDictionaryLab.java
@@ -25,36 +25,52 @@ public class NccrDictionaryLab {
     public static void main(String[] args) throws IOException {
         NaaccrApiClient client = NaaccrApiClient.getInstance();
 
+        // possible Semantic_Type:  "numeric", "boolean", "string", "lookup_value", "numeric_relation"
+
         String version = "23";
         List<String> itemsToFetch = Arrays.asList("ageAtDiagnosis", "ajccId", "behaviorIcdO2", "behaviorCodeIcdO3", "brainMolecularMarkers", "breslowTumorThickness", "cocAccreditedFlag",
                 "csExtension", "csLymphNodes", "csLymphNodesEval", "csMetsAtDx", "csMetsEval");
 
-
         // the notes are provided by them, I didn't want to lose them, so I am "re-injecting" them
         Map<String, String> notes = new HashMap<>();
-        notes.put("ageAtDiagnosis", "This data item will be used to create an efficient process for running TNM Edits. Each Site-Specific Data Item (SSDI) applies only to selected primary sites,histologies, and years of diagnosis. Depending on applicability and standard-setter requirements, SSDIs may be left blank.");
-        notes.put("ajccId", "This data item will be used to create an efficient process for running TNM Edits.Each Site-Specific Data Item (SSDI) applies only to selected primary sites histologies, and years of diagnosis. Depending on applicability and standard-setter requirements, SSDIs may beleft blank.");
+        notes.put("ajccId",
+                "This data item will be used to create an efficient process for running TNM Edits.Each Site-Specific Data Item (SSDI) applies only to selected primary sites histologies, and years of diagnosis. Depending on applicability and standard-setter requirements, SSDIs may beleft blank.");
         notes.put("brainMolecularMarkers", "Collection of these clinically important brain cancer subtypes has been recommended by CBTRUS.");
         notes.put("breslowTumorThickness", "Breslow Tumor Thickness is a Registry Data Collection Variable in AJCC. It was previously collected as Melanoma Skin, CS SSF# 1.");
-        notes.put("cocAccreditedFlag", "CoC-accredited facilities are required to collect certain data items including TNM staging. It is burdensome for central registries to maintain a list of accredited facilities, and the list changes frequently. The flag is a means of incorporating the accredited status into abstracts at the time of abstraction by someone who has knowledge of the status. The flag thus simplifies validating that required items have been abstracted by CoC-accredited facilities. The flag also allows cases to be stratified during analyses to identify those never seen at a CoC-accredited facility; e.g., percentage of all cases seen in at least one CoC-accredited facility, evaluation of outcomes by facility status. NPCR will use this flag for facility status stratification.");
+        notes.put("cocAccreditedFlag",
+                "CoC-accredited facilities are required to collect certain data items including TNM staging. It is burdensome for central registries to maintain a list of accredited facilities, and the list changes frequently. The flag is a means of incorporating the accredited status into abstracts at the time of abstraction by someone who has knowledge of the status. The flag thus simplifies validating that required items have been abstracted by CoC-accredited facilities. The flag also allows cases to be stratified during analyses to identify those never seen at a CoC-accredited facility; e.g., percentage of all cases seen in at least one CoC-accredited facility, evaluation of outcomes by facility status. NPCR will use this flag for facility status stratification.");
         notes.put("csExtension", "Tumor extension at diagnosis is a prognostic indicator used by Collaborative Staging to derive some TNM-T codes and some SEER Summary Stage codes.");
         notes.put("csLymphNodes", "The involvement of specific regional lymph nodes is a prognostic indicator used by Collaborative Staging to derive some TNM-N codes and SEER Summary Stage codes.");
-        notes.put("csLymphNodesEval", "This data item is used by Collaborative Staging to describe whether the staging basis for the TNM-N code is clinical or pathological and to record applicable prefix and suffix descriptors used with TNM staging.");
-        notes.put("csMetsAtDx", "The presence of metastatic disease at diagnosis is an independent prognostic indicator, and it is used by Collaborative Staging to derive TNM-M codes and SEER Summary Stage codes.");
-        notes.put("csMetsEval", "This data item is used by Collaborative Staging to describe whether the staging basis for the TNM-M code is clinical or pathological and to record applicable prefix and suffix descriptors used with TNM staging.");
+        notes.put("csLymphNodesEval",
+                "This data item is used by Collaborative Staging to describe whether the staging basis for the TNM-N code is clinical or pathological and to record applicable prefix and suffix descriptors used with TNM staging.");
+        notes.put("csMetsAtDx",
+                "The presence of metastatic disease at diagnosis is an independent prognostic indicator, and it is used by Collaborative Staging to derive TNM-M codes and SEER Summary Stage codes.");
+        notes.put("csMetsEval",
+                "This data item is used by Collaborative Staging to describe whether the staging basis for the TNM-M code is clinical or pathological and to record applicable prefix and suffix descriptors used with TNM staging.");
 
         NccrDictionary dictionary = new NccrDictionary();
-        dictionary.setNaaccrVersion(version + "0");
         dictionary.setElements(new ArrayList<>());
 
         for (String itemId : itemsToFetch) {
             NaaccrDataItem item = client.getDataItem(version, itemId);
 
             NccrDictionaryElement element = new NccrDictionaryElement();
+            element.setColumnNameAtSource(item.getXmlNaaccrId());
+            NccrDictionaryDataType dataType = new NccrDictionaryDataType();
+            dataType.setFieldLength(item.getItemLength());
+            if (item.getAllowedCodes() != null && !item.getAllowedCodes().isEmpty())
+                dataType.setSemanticType("lookup_value");
+            else if ("digits".equals(item.getItemDataType()))
+                dataType.setSemanticType("numeric");
+            else
+                dataType.setSemanticType("string");
+            if (item.getSourceOfStandard() != null)
+                dataType.setAbbreviatedSourceVocabulary(item.getSourceOfStandard());
+            element.setColumnDataTypeAtSource(dataType);
             element.setItemName(item.getItemName());
             element.setItemId(item.getXmlNaaccrId());
             element.setItemNumber(item.getItemNumber().toString());
-            element.setNaaccrLink("https://apps.naaccr.org/data-dictionary/api/1.0/data_item/" + version + "/" + item.getXmlNaaccrId());
+            element.setInternetLink("https://apps.naaccr.org/data-dictionary/api/1.0/data_item/" + version + "/" + item.getXmlNaaccrId());
             element.setItemDescription(item.getDescription());
             element.setRationale(notes.get(itemId));
 
@@ -93,19 +109,8 @@ public class NccrDictionaryLab {
 
     public static class NccrDictionary {
 
-        @JsonProperty("NAACCR_Version")
-        private String _naaccrVersion;
-
         @JsonProperty("Dictionary_Elements")
         private List<NccrDictionaryElement> _elements;
-
-        public String getNaaccrVersion() {
-            return _naaccrVersion;
-        }
-
-        public void setNaaccrVersion(String naaccrVersion) {
-            _naaccrVersion = naaccrVersion;
-        }
 
         public List<NccrDictionaryElement> getElements() {
             return _elements;
@@ -118,6 +123,12 @@ public class NccrDictionaryLab {
 
     public static class NccrDictionaryElement {
 
+        @JsonProperty("Column_Name_at_Source")
+        private String _columnNameAtSource;
+
+        @JsonProperty("Column_DataType_at_Source")
+        private NccrDictionaryDataType _columnDataTypeAtSource;
+
         @JsonProperty("Item_Name")
         private String _itemName;
 
@@ -127,8 +138,8 @@ public class NccrDictionaryLab {
         @JsonProperty("Item_Number")
         private String _itemNumber;
 
-        @JsonProperty("NAACCR_Link")
-        private String _naaccrLink;
+        @JsonProperty("Internet_Link")
+        private String _internetLink;
 
         @JsonProperty("Item_Description")
         private String _itemDescription;
@@ -138,6 +149,22 @@ public class NccrDictionaryLab {
 
         @JsonProperty("Permissible_Values")
         private List<NccrDictionaryElementValue> _permissibleValues;
+
+        public String getColumnNameAtSource() {
+            return _columnNameAtSource;
+        }
+
+        public void setColumnNameAtSource(String columnNameAtSource) {
+            _columnNameAtSource = columnNameAtSource;
+        }
+
+        public NccrDictionaryDataType getColumnDataTypeAtSource() {
+            return _columnDataTypeAtSource;
+        }
+
+        public void setColumnDataTypeAtSource(NccrDictionaryDataType columnDataTypeAtSource) {
+            _columnDataTypeAtSource = columnDataTypeAtSource;
+        }
 
         public String getItemName() {
             return _itemName;
@@ -163,12 +190,12 @@ public class NccrDictionaryLab {
             _itemNumber = itemNumber;
         }
 
-        public String getNaaccrLink() {
-            return _naaccrLink;
+        public String getInternetLink() {
+            return _internetLink;
         }
 
-        public void setNaaccrLink(String naaccrLink) {
-            _naaccrLink = naaccrLink;
+        public void setInternetLink(String internetLink) {
+            _internetLink = internetLink;
         }
 
         public String getItemDescription() {
@@ -193,6 +220,42 @@ public class NccrDictionaryLab {
 
         public void setPermissibleValues(List<NccrDictionaryElementValue> permissibleValues) {
             _permissibleValues = permissibleValues;
+        }
+    }
+
+    public static class NccrDictionaryDataType {
+
+        @JsonProperty("Field_Length")
+        private Integer _fieldLength;
+
+        @JsonProperty("Semantic_Type")
+        private String _semanticType;
+
+        @JsonProperty("Abbreviated_Source_Vocabulary")
+        private String _abbreviatedSourceVocabulary;
+
+        public Integer getFieldLength() {
+            return _fieldLength;
+        }
+
+        public void setFieldLength(Integer fieldLength) {
+            _fieldLength = fieldLength;
+        }
+
+        public String getSemanticType() {
+            return _semanticType;
+        }
+
+        public void setSemanticType(String semanticType) {
+            _semanticType = semanticType;
+        }
+
+        public String getAbbreviatedSourceVocabulary() {
+            return _abbreviatedSourceVocabulary;
+        }
+
+        public void setAbbreviatedSourceVocabulary(String abbreviatedSourceVocabulary) {
+            _abbreviatedSourceVocabulary = abbreviatedSourceVocabulary;
         }
     }
 
